45924ea6385bdd42ff835961843c8c18d9166153
Albert Wang <albertyw@mit.edu>
2013-11-05 23:37:43 -0500

    Added comments model

--- blog/db/schema.rb
+++ blog/db/schema.rb
@@ -9,15 +9,25 @@
 # from scratch. The latter is a flawed and unsustainable approach (the more migrations
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
-# It's strongly recommended to check this file into your version control system.
+# It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131028043529) do
+ActiveRecord::Schema.define(version: 20131106042928) do
 
-  create_table "posts", :force => true do |t|
+  create_table "comments", force: true do |t|
+    t.string   "commenter"
+    t.text     "body"
+    t.integer  "post_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "comments", ["post_id"], name: "index_comments_on_post_id"
+
+  create_table "posts", force: true do |t|
     t.string   "title"
     t.string   "text"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
